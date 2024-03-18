2014ac637ce1f6eda55970b15306112f7be341f2
Lethabo Kgwadi <lethabokgwadi@gmail.com>
2024-03-18 03:14:42 -0700

    practice.py
    Functions basics.

---
+++ examples.py
@@ -0,0 +1,17 @@
+#var1 = 1
+#var2 = 2
+
+#def someFunc(var1):
+    #print(f'{var1},{var2}')
+#someFunc(3)
+
+#def someFunc(func):
+    #print(func(5) + 2)
+#someFunc(lambda x: x * 3)
+
+#var1 = 456 % 10
+#print(var1)
+
+var = [1,2,3]
+var.append(4)
+print(var)
